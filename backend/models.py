89624df344bef87b2d7328441b9c93dbd3f4a6ad
Alex Kiborgok <borgokalek14@gmail.com>
2022-06-26 21:34:52 +0300

    reconfigure dot-env

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,9 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
 DB_USER = os.getenv('DB_USER')
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,10 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -18,12 +14,12 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
-        self.DB_USER = os.getenv('DB_USER', 'postgres')
-        self.DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+        self.DB_USER = os.getenv('DB_USER')
+        self.DB_PASSWORD = os.getenv('DB_PASSWORD')
         self.DB_NAME = os.getenv('DB_NAME', 'test_trivia')
-        self.DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
+        self.database_path = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
             self.DB_USER, self.DB_PASSWORD, self.DB_HOST, self.DB_NAME)
-        setup_db(self.app, self.DB_PATH)
+        setup_db(self.app, self.database_path)
 
         # binds the app to the current context
         with self.app.app_context():
