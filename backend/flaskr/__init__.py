f2c6d40d18fca15e1704e593563640d29821cf1a
Alex Kiborgok <borgokalek14@gmail.com>
2022-06-26 21:20:27 +0300

    add 405 error handler

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -299,4 +299,12 @@ def create_app(test_config=None):
             "message": 'bad request'
         }), 400
 
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": 'method not allowed'
+        }), 405
+
     return app
