8f2a960d433562a804ccb42bd9355e4d8d6fe31a
kiborgok <borgokalek14@gmail.com>
2022-06-27 06:20:30 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -9,7 +9,7 @@ load_dotenv()
 DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_NAME = os.getenv('DB_NAME', 'trivia')
+DB_NAME = os.getenv('DB_NAME')
 DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 
 db = SQLAlchemy()
